385f45837aaa012a13a42ef20df9e3274360de11
ymmrealise <sfenton4@kent.edu>
2020-10-01 03:10:09 +0000

    caught up with 9.23

--- web 1/bottle_app.py
+++ web 1/bottle_app.py
@@ -7,7 +7,7 @@ from bottle import get, post, request, template, redirect
 ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ.keys()
 
 if ON_PYTHONANYWHERE:
-    from bottle import default_app 
+    from bottle import default_app
 else:
     from bottle import run, debug
 
@@ -20,6 +20,16 @@ def get_show_list():
     result = cursor.fetchall()
     cursor.close()
     return template("show_list", rows=result)
+
+
+@get('/set_status/<id:int>/<value:int>')
+def get_set_status(id, value):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("update todo set status=? where id=?", (value, id,))
+    connection.commit()
+    cursor.close()
+    redirect('/')
 
 
 @get('/new_item')
@@ -38,10 +48,39 @@ def post_new_item():
     redirect('/')
 
 
+@get('/update_item/<id:int>')
+def get_update_item(id):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("select * from todo where id=?", (id,))
+    result = cursor.fetchall()
+    cursor.close()
+    return template("update_item", row=result[0])
+
+
+@post('/update_item')
+def post_update_item():
+    id = int(request.forms.get("id").strip())
+    updated_item = request.forms.get("updated_item").strip()
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("update todo set task=? where id=?", (updated_item, id,))
+    connection.commit()
+    cursor.close()
+    redirect('/')
+
+@get('/delete_item/<id:int>')
+def get_delete_item(id):
+    print("we want to delete #" + str(id))
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("delete from todo where id=?", (id,))
+    connection.commit()
+    cursor.close()
+    redirect('/')
+
 if ON_PYTHONANYWHERE:
     application = default_app()
 else:
     debug(True)
     run(host="localhost", port=8080)
-
-
